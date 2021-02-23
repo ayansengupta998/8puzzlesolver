26f5829349a2cdfd81eefb0580f4cadfa1e7101a
achupanikath <achupanikath@gmail.com>
2021-02-23 16:20:37 -0500

    A star search completely implemented

--- solver.py
+++ solver.py
@@ -51,8 +51,8 @@ def solve_puzzle(start_state, strategy):
     elif strategy == "astar-h2":
         results = astar_h2(start_state)
     elif strategy == "astar-h3":
-        results = astar_h3(start_state)   
-    
+        results = astar_h3(start_state)
+
     
     return results
 def bfs_search(start_state):
@@ -110,63 +110,214 @@ def astar_h1(start_state):
                'expanded_count': 0,
     }
     q =pdqpq.PriorityQueue()#define a priority queue q which is empty
+    parents =dict()
+    parents[start_state] = (0,0,'')
     #define two sets, frontier and explored
-    frontier = set()
-    explored = []
-    frontier.add(start_state)
-    results['path'].append(('start',start_state))
-    recurser(start_state, q, results, frontier, explored,'')
+    frontier = dict()
+    explored = dict()
+    # frontier[start_state] = 0
+    # results['path'].append(('start',start_state))
+    current_state = start_state
+    # recurser(start_state, q, results, frontier, explored,'' )
+    G = 0
+    while((str(current_state) not in explored) or not q.empty()):
+        if str(GOAL_STATE) == str(current_state):
+            break
+        if str(current_state) not in explored:
+            explored[str(current_state)] = ''
+            # print("EXPLORED:",explored)
+            #explore the unexplored states
+            successors = current_state.successors()
+            for key in successors:
+                #get each successor
+                new_state = successors[key]
+                h = heuristic(new_state, 1)
+                # print(h)
+                g = cost(current_state, new_state)
+                # print(g)
+                G = parents[current_state][1] + g
+                f = G + h    
+                if new_state not in frontier:
+                    #add every expanded state to the frontier
+                    frontier[new_state] = g
+                    # print("FRONTIER:", frontier)
+                    q.add((new_state, key), f)
+                    #add child/parent
+                    parents[new_state] = (current_state,G, key)
+                    # print(parents[new_state][0])
+                else:
+                    if parents[new_state][1] > G:
+                        parents[new_state] = (current_state, G, key)
+                    #update parent/child/direction if Total cost of child < current total cost
+        node = q.pop()
+        # recurser(node[0],q,results,frontier, explored,node[1])
+        current_state = node[0]
+    #path get traverse dictionary
+    if str(GOAL_STATE) == str(current_state):
+        results['path_cost'] = parents[GOAL_STATE][1]
+        key = GOAL_STATE
+        move = parents[key][2]
+        results['path'].append((move, key))
+        # results['path_cost']+=frontier[parents[key][1]]
+        while(parents[key][0] != start_state):
+            key = parents[key][0]
+            move = parents[key][2]
+            results['path'].insert(0,(move, key))
+            # results['path_cost']+=frontier[parents[key][1]]
+        parent = start_state
+        move = 'start'
+        results['path'].insert(0,(move, parent))
+
     results['frontier_count'] = len(frontier)
     results['expanded_count'] = len(explored)
     return results
 
-def recurser(current_state, q, results, frontier, explored, move):
-    if str(GOAL_STATE) == str(current_state) or (str(current_state) in explored and q.empty()):
-        return
-    if str(current_state) not in explored:
-        explored.append(str(current_state))
-        #explore the unexplored states
-        successors = current_state.successors()
-        for key in successors:
-            #get each successor
-            new_state = successors[key]
-            if new_state not in frontier:
-                h = heuristic(new_state, 1)
-                g = cost(current_state, new_state)
-                f = g + h
-                #add every expanded state to the frontier
-                frontier.add(new_state)
-                q.add((new_state, key), f)
-    print("Explored is", explored)
-    print("Queue is ",q)
-    results['path'].append((q.pq[0][2][1],q.pq[0][2][0]))
-    moveCost = q.pq[0][0]
-    results['path_cost'] += moveCost
-    node = q.pop()
-    recurser(node[0],q,results,frontier, explored,node[1])
 
 def astar_h2(start_state):
     results = {'path' : [],
                'path_cost':0,
                'frontier_count': 0,
                'expanded_count': 0,
-    
-    }    
-    q =pdqpq.PriorityQueue()
-    heuristic(start_state, 2)
+    }
+    q =pdqpq.PriorityQueue()#define a priority queue q which is empty
+    parents =dict()
+    parents[start_state] = (0,0,'')
+    #define two sets, frontier and explored
+    frontier = dict()
+    explored = dict()
+    # frontier[start_state] = 0
+    # results['path'].append(('start',start_state))
+    current_state = start_state
+    # recurser(start_state, q, results, frontier, explored,'' )
+    G = 0
+    while((str(current_state) not in explored) or not q.empty()):
+        if str(GOAL_STATE) == str(current_state):
+            break
+        if str(current_state) not in explored:
+            explored[str(current_state)] = ''
+            # print("EXPLORED:",explored)
+            #explore the unexplored states
+            successors = current_state.successors()
+            for key in successors:
+                #get each successor
+                new_state = successors[key]
+                h = heuristic(new_state, 2)
+                # print(h)
+                g = cost(current_state, new_state)
+                # print(g)
+                G = parents[current_state][1] + g
+                f = G + h    
+                if new_state not in frontier:
+                    #add every expanded state to the frontier
+                    frontier[new_state] = g
+                    # print("FRONTIER:", frontier)
+                    q.add((new_state, key), f)
+                    #add child/parent
+                    parents[new_state] = (current_state,G, key)
+                    # print(parents[new_state][0])
+                else:
+                    if parents[new_state][1] > G:
+                        parents[new_state] = (current_state, G, key)
+                    #update parent/child/direction if Total cost of child < current total cost
+        node = q.pop()
+        # recurser(node[0],q,results,frontier, explored,node[1])
+        current_state = node[0]
+    #path get traverse dictionary
+    if str(GOAL_STATE) == str(current_state):
+        results['path_cost'] = parents[GOAL_STATE][1]
+        key = GOAL_STATE
+        move = parents[key][2]
+        results['path'].append((move, key))
+        # results['path_cost']+=frontier[parents[key][1]]
+        while(parents[key][0] != start_state):
+            key = parents[key][0]
+            move = parents[key][2]
+            results['path'].insert(0,(move, key))
+            # results['path_cost']+=frontier[parents[key][1]]
+        parent = start_state
+        move = 'start'
+        results['path'].insert(0,(move, parent))
 
+    results['frontier_count'] = len(frontier)
+    results['expanded_count'] = len(explored)
     return results
 
-    
+def flip(move):
+    if move == 'left':
+        return 'right'
+    elif move =='right':
+        return 'left'
+    elif move =='up':
+        return 'down'
+    else:
+        return 'up'
 def astar_h3(start_state):
     results = {'path' : [],
                'path_cost':0,
                'frontier_count': 0,
                'expanded_count': 0,
-    
-    }    
-    q =pdqpq.PriorityQueue()
-    heuristic(start_state, 3)
+    }
+    q =pdqpq.PriorityQueue()#define a priority queue q which is empty
+    parents =dict()
+    parents[start_state] = (0,0,'')
+    #define two sets, frontier and explored
+    frontier = dict()
+    explored = dict()
+    # frontier[start_state] = 0
+    # results['path'].append(('start',start_state))
+    current_state = start_state
+    # recurser(start_state, q, results, frontier, explored,'' )
+    G = 0
+    while((str(current_state) not in explored) or not q.empty()):
+        if str(GOAL_STATE) == str(current_state):
+            break
+        if str(current_state) not in explored:
+            explored[str(current_state)] = ''
+            # print("EXPLORED:",explored)
+            #explore the unexplored states
+            successors = current_state.successors()
+            for key in successors:
+                #get each successor
+                new_state = successors[key]
+                h = heuristic(new_state, 3)
+                # print(h)
+                g = cost(current_state, new_state)
+                # print(g)
+                G = parents[current_state][1] + g
+                f = G + h    
+                if new_state not in frontier:
+                    #add every expanded state to the frontier
+                    frontier[new_state] = g
+                    # print("FRONTIER:", frontier)
+                    q.add((new_state, key), f)
+                    #add child/parent
+                    parents[new_state] = (current_state,G, key)
+                    # print(parents[new_state][0])
+                else:
+                    if parents[new_state][1] > G:
+                        parents[new_state] = (current_state, G, key)
+                    #update parent/child/direction if Total cost of child < current total cost
+        node = q.pop()
+        # recurser(node[0],q,results,frontier, explored,node[1])
+        current_state = node[0]
+    #path get traverse dictionary
+    if str(GOAL_STATE) == str(current_state):
+        results['path_cost'] = parents[GOAL_STATE][1]
+        key = GOAL_STATE
+        move = parents[key][2]
+        results['path'].append((move, key))
+        # results['path_cost']+=frontier[parents[key][1]]
+        while(parents[key][0] != start_state):
+            key = parents[key][0]
+            move = parents[key][2]
+            results['path'].insert(0,(move, key))
+            # results['path_cost']+=frontier[parents[key][1]]
+        parent = start_state
+        move = 'start'
+        results['path'].insert(0,(move, parent))
+
+    results['frontier_count'] = len(frontier)
+    results['expanded_count'] = len(explored)
     return results
 
 #helper funciton to calculate the 3 different heuristics as selected
@@ -198,7 +349,7 @@ def heuristic(state, h):
                 y = state.find(tile)[1]
                 xg= GOAL_STATE.find(tile)[0]
                 yg = GOAL_STATE.find(tile)[1]
-                val += (abs(x-xg) + abs(y-yg)) * (int(tile)**2)
+                val += ((abs(x-xg) + abs(y-yg)) * (int(tile)**2))
     return val
 
 def cost(current_state, next_state):
